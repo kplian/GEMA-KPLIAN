cbcd80bf1a93c2885adcb40ffe8df311310044a9
Rensi Arteaga Copar <rensi@kplian.com>
2012-12-27 06:40:29 -0400

    prueba calendario planificado

--- sis_mantenimiento/control/ACTCalendarioPlanificado.php
+++ sis_mantenimiento/control/ACTCalendarioPlanificado.php
@@ -68,7 +68,7 @@ class ACTCalendarioPlanificado extends ACTbase{
 		$this->objParam->defecto('dir_ordenacion','asc');
 		if($this->objParam->getParametro('tipoReporte')=='excel_grid' || $this->objParam->getParametro('tipoReporte')=='pdf_grid'){
 			$this->objReporte = new Reporte($this->objParam,$this);
-			$this->res = $this->objReporte->generarReporteListado('FuncionesMantenimiento','listarCalendarioPlanificadoDet');
+			$this->res = $this->objReporte->generarReporteListado('MODCalendarioPlanificado','listarCalendarioPlanificadoDet');
 
 		} else{
 			$this->objFunc=$this->create('MODCalendarioPlanificado');	
