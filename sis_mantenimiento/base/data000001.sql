e775dd482e89c2033fa3efc0c9f51507ab78f16e
Rodrigo Chumacero <rodrigo@kplian.com>
2013-01-14 15:16:43 -0400

    Organización de los scripts en archivos data, patch y dependencies

--- sis_mantenimiento/base/data000001.sql
+++ sis_mantenimiento/base/data000001.sql
@@ -826,24 +826,4 @@ select pxp.f_add_catalog('GEM','torden_trabajo_sol__importancia','Alta');
 select pxp.f_add_catalog('GEM','torden_trabajo_sol__importancia','Media');
 select pxp.f_add_catalog('GEM','torden_trabajo_sol__importancia','Baja');
 
-INSERT INTO segu.tsubsistema ( codigo, nombre, fecha_reg, prefijo, estado_reg, nombre_carpeta, id_subsis_orig)
-VALUES ('GEN', 'Sistema Generador de Codigo', '2009-11-02', 'GEN', 'activo', 'generador', NULL);
-
-
-select pxp.f_insert_tfuncion ('ft_tabla_sel', 'Funcion para tabla', 'GEN');
-select pxp.f_insert_tfuncion ('ft_tabla_ime', 'Funcion para tabla', 'GEN');
-select pxp.f_insert_tfuncion ('ft_columna_ime', 'Funcion para tabla', 'GEN');
-select pxp.f_insert_tfuncion ('ft_columna_sel', 'Funcion para tabla', 'GEN');
-select pxp.f_insert_tfuncion ('ft_esquema_sel', 'Funcion para tabla', 'GEN');
-select pxp.f_insert_tgui ('Columnas', 'Registro de las Columnas de las tablas', 'COL', 'no', 1, 'sis_generador/vista/columna/columna.js', 4, '', 'columna', 'GEN');
-select pxp.f_insert_tgui ('Procesos', '', 'PROCGEN', 'si', 2, '', 2, '', '', 'GEN');
-select pxp.f_insert_tgui ('GEN', 'Generador de Código', 'GEN', 'si', 3, '', 1, '../../../lib/imagenes/gen32x32.png', '', 'GEN');
-select pxp.f_insert_tgui ('Generador', 'Registro de las tablas', 'TABLA', 'si', 1, 'sis_generador/vista/tabla/tabla.js', 3, '', 'tabla', 'GEN');
-select pxp.f_insert_testructura_gui ('GEN', 'SISTEMA');
-select pxp.f_insert_testructura_gui ('PROCGEN', 'GEN');
-select pxp.f_insert_testructura_gui ('TABLA', 'PROCGEN');
-select pxp.f_insert_testructura_gui ('COL', 'TABLA');
-
-
-
-/***********************************F-SCP-RAC-GEN-0-02/01/2013****************************************/
+/***********************************F-SCP-RCM-GEM-36-02/01/2013****************************************/
